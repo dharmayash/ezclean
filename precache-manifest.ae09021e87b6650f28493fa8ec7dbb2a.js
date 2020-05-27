398ddae4bf38e1315e406b42b0b48a82073ae32b
YASPAL <vcarealldev@gmail.com>
2020-05-27 11:46:21 +0530

    first build

---
+++ precache-manifest.ae09021e87b6650f28493fa8ec7dbb2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7eae921d8b37ceb89d0c966be765893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d2e1d13615fa81e73d",
+    "url": "/static/css/2.60d9b82a.chunk.css"
+  },
+  {
+    "revision": "10c443c1ac11176698a1",
+    "url": "/static/css/main.ec3adb3c.chunk.css"
+  },
+  {
+    "revision": "12d2e1d13615fa81e73d",
+    "url": "/static/js/2.1e824ae3.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.1e824ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c443c1ac11176698a1",
+    "url": "/static/js/main.01919354.chunk.js"
+  },
+  {
+    "revision": "0fb66249a8f307507309",
+    "url": "/static/js/runtime-main.6da9b286.js"
+  }
+]);
