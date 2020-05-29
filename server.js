46a36da176d5fadf738cb1abc7efd6cfa76c96b8
dharmayash <yashpalgkp1991@gmail.com>
2020-05-29 09:04:56 +0530

    node server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const path=require('path');
+const port=8000;
+const app=express();
+
+app.use(express.static(__dirname));
+
+app.listen(port);
+
+console.log('Server Started');
